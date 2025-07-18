4f8fdf605a8f403cb3f5a98f7ed17e6f85631e22
MiguelMoreiraDev <contatomigueldev@gmail.com>
2025-07-18 20:02:05 -0300

    ✨ Feat: create auth controllers
    Create auth controllers

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,5 +1,9 @@
 import express from 'express'
+import AuthController from '@/controllers/auth.controller.ts'
 
 const routes = express.Router()
+
+routes.get('/login', AuthController.login)
+routes.get('/register', AuthController.register)
 
 export default routes
