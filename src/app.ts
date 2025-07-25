103434cfbcf65e44888978d80186f8a26c65973b
MiguelMoreiraDev <contatomigueldev@gmail.com>
2025-07-25 17:33:50 -0300

    🩹 Fix: indent width fix

--- src/app.ts
+++ src/app.ts
@@ -11,14 +11,14 @@ app.use(express.json())
 const port = config.PORT || 3333
 
 const handleInit = async () => {
-    try {
-        await handleConnectDatabase()
-        app.listen(port, () => {
-            console.log(`Server running on PORT: ${port}`)
-        })
-    } catch (error) {
-        console.error(error)
-    }
+  try {
+    await handleConnectDatabase()
+    app.listen(port, () => {
+      console.log(`Server running on PORT: ${port}`)
+    })
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 handleInit()
